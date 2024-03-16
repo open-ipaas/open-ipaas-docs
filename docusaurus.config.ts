df36b87d1cf59d1525a1bf280838a99b0b966af2
zsan3653 <zsan3653@gmail.com>
2024-03-16 21:13:55 +0800

    change editor url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/open-ipaas/open-ipaas.github.io/tree/main/docs/",
+            "https://github.com/open-ipaas/open-ipaas.github.io/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/open-ipaas/open-ipaas.github.io/tree/main/blog/",
+            "https://github.com/open-ipaas/open-ipaas.github.io/tree/main/",
         },
         theme: {
           customCss: "./src/css/custom.css",
