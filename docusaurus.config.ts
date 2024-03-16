fe6dc23091378d16d3962f2d51238017602ad33c
zsan3653 <zsan3653@gmail.com>
2024-03-16 20:34:18 +0800

    change url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/open-ipaas",
           label: "GitHub",
           position: "right",
         },
